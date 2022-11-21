b7ce617b02e91c7ef932393290a2c574aa777843
james1BadCreeper <78631346+james1BadCreeper@users.noreply.github.com>
2022-11-21 11:21:50 +0800

    code update

---
+++ work.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+struct Matrix {
+    int a[105][105];
+};
+
+int n, m, t, a, b;
+
+int main(void)
+{
+    scanf("%d%d%d%d%d", &n, &m, &t, &a, &b);
+    return 0;
+}
