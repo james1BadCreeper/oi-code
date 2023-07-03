e9e19d9b4eef8405ce331e418f0243d88ed7006c
james1BadCreeper <78631346+james1BadCreeper@users.noreply.github.com>
2023-07-03 23:47:12 +0000

    CF1288E

--- work.cpp
+++ work.cpp
@@ -1,36 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std; 
-typedef long long i64; 
 
-int n; i64 ans; 
-int a[1005], p[1005]; 
-mt19937 Rand(19120623); 
-
-int rndint(int l, int r) { return uniform_int_distribution<>(l, r)(Rand); }
-inline double rnddb(double l, double r) { return uniform_real_distribution<>(l, r)(Rand); }
-
-void calc(int x, int y) {
-
-}
-
-void SA(double T, const double ET, const double delta) {
-    for (int i = 1; i <= n; ++i) calc(i, i); 
-    while (T > ET) {
-        int x = rndint(1, n), y = rndint(1, n); 
-        i64 res = ans; 
-        int px = p[x], py = p[y]; 
-        calc(x, py); calc(y, px); 
-        if (res > ans || exp((ans - res) / T) <= rnddb(0, 1)) ; 
-        else ans = res, swap(p[x], p[y]); 
-        T *= delta; 
-    }
-    for (int i = 1; i <= n; ++i) printf("%d ", p[i]);
-    putchar('\n');  
-}
+int n, m; 
+int nxt[300005], hd, pre[300005]; 
+int mn[300005], mx[300005], rnk[300005]; 
 
 int main(void) {
-    scanf("%d", &n); 
-    for (int i = 1; i <= n; ++i) scanf("%d", a + i); 
-    SA(1e15, -1e15, 0.999999); 
+    scanf("%d%d", &n, &m); hd = 1; 
+    for (int i = 1; i < n; ++i) nxt[i] = i + 1, pre[i + 1] = i; 
+    for (int i = 1; i <= n; ++i) mn[i] = mx[i] = i; 
+    while (m--) {
+        int b; scanf("%d", &b); 
+        if (b == hd) continue; 
+        int a = pre[b], c = nxt[b]; 
+        nxt[a] = c; 
+        if (c) pre[c] = a; 
+        nxt[b] = hd, pre[b] = 0, hd = b; 
+        
+    }
     return 0; 
 }
