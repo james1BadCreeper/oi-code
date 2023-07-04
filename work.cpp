8d90e64ead0c36054971bf6bf4ad580abd478a49
james1BadCreeper <78631346+james1BadCreeper@users.noreply.github.com>
2023-07-04 00:24:26 +0000

    6489

--- work.cpp
+++ work.cpp
@@ -2,21 +2,11 @@
 using namespace std; 
 
 int n, m; 
-int nxt[300005], hd, pre[300005]; 
-int mn[300005], mx[300005], rnk[300005]; 
+int a[100005]; 
 
 int main(void) {
-    scanf("%d%d", &n, &m); hd = 1; 
-    for (int i = 1; i < n; ++i) nxt[i] = i + 1, pre[i + 1] = i; 
-    for (int i = 1; i <= n; ++i) mn[i] = mx[i] = i; 
-    while (m--) {
-        int b; scanf("%d", &b); 
-        if (b == hd) continue; 
-        int a = pre[b], c = nxt[b]; 
-        nxt[a] = c; 
-        if (c) pre[c] = a; 
-        nxt[b] = hd, pre[b] = 0, hd = b; 
-        
-    }
+    scanf("%d%d", &n, &m); 
+    for (int i = 1; i <= n; ++i) scanf("%d", a + i); 
+    
     return 0; 
 }
